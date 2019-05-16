cfa4ef56fa7ca63088f87730ebdb0bf636cced9e
Rasmus Einarsson <mr@rasmuseinarsson.se>
2019-05-16 18:30:38 +0200

    Create CLI command list-tables

--- eust/cli.py
+++ eust/cli.py
@@ -28,5 +28,39 @@ def nuts_codes():
     eust.download_nuts_codes()
 
 
+def _iter_table_versions_pairs(latest):
+    for table in eust.list_tables():
+        versions = eust.list_table_versions(table)
+        if latest:
+            versions = versions[-1:]
+
+        for version in versions:
+            yield (table, version)
+
+@main.command()
+@click.option(
+    '--versions/--no-versions',
+    is_flag=True,
+    default=True,
+    help='Print version information. (--no-versions implies --latest)',
+)
+@click.option(
+    '--latest',
+    is_flag=True,
+    default=False,
+    help='Print the latest available version only.',
+)
+def list_tables(versions, latest):
+    if not versions:
+        latest = True
+
+
+    for table, version in _iter_table_versions_pairs(latest):
+        if not versions:
+            click.echo(table)
+        else:
+            click.echo(f'{table:<20} {version}')
+
+
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
