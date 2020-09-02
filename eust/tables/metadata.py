ea9b4e96c5b22d39ea5b3ba689a196b519f12c17
Rasmus Einarsson <mr@rasmuseinarsson.se>
2020-09-02 18:07:40 +0200

    Add support for pandasdmx v1 (fix #7)

--- eust/tables/metadata.py
+++ eust/tables/metadata.py
@@ -12,12 +12,16 @@ from eust.core import (
     PathLike,
     )
 
+# time dimension does not appear in metadata
+_EXCLUDED_DIMENSIONS = {"TIME"}
 
 def _is_header_row(d):
     return (d['dimension'] == d['code']) & (d['dimension'] == d['label'])
 
 
-def _get_dimensions(codelist):
+def _get_dimensions_0_9(structure_message):
+    # pandasdmx v0.9
+    codelist = structure_message["codelist"]
     dimensions = (
         codelist
         [codelist['dim_or_attr'] == 'D']
@@ -35,8 +39,41 @@ def _get_dimensions(codelist):
 
     return dimensions
 
+def _get_dimensions_1(structure_message):
+    # pandasdmx v1
+    dsd, = structure_message.structure.values() # unpack exactly 1 table
+    dimension_names = pandasdmx.to_pandas(dsd.dimensions)
 
-def _get_attributes(codelist):
+    codelist = pandasdmx.to_pandas(structure_message.codelist)
+
+    dimension_items = {
+        name.lower(): codelist[f"CL_{name}"]
+        for name in dimension_names
+        if name not in _EXCLUDED_DIMENSIONS
+    }
+
+    dimensions = (
+        pd.concat(dimension_items)
+        .rename_axis(["dimension", "code"])
+        .rename("label")
+        .to_frame()
+        )
+
+    return dimensions
+
+def _get_dimensions(codelist):
+    if pandasdmx.__version__.startswith("0.9"):
+        return _get_dimensions_0_9(codelist)
+
+    if pandasdmx.__version__.startswith("1."):
+        return _get_dimensions_1(codelist)
+
+    raise Exception(f"pandasdmx version is {pandasdmx.__version__}")
+
+
+def _get_attributes_0_9(structure_message):
+    # pandasdmx v0.9
+    codelist = structure_message["codelist"]
     attributes = (
         codelist
         [codelist['dim_or_attr'] == 'A']
@@ -53,6 +90,40 @@ def _get_attributes(codelist):
     return attributes
 
 
+def _get_attributes_1(structure_message):
+    # pandasdmx v1
+    dsd, = structure_message.structure.values() # unpack exactly 1 table
+    attribute_names = [attr.id for attr in dsd.attributes]
+
+    codelist = pandasdmx.to_pandas(structure_message.codelist)
+
+    attribute_items = {
+        name.lower(): codelist[f"CL_{name}"]
+        for name in attribute_names
+    }
+
+    attributes = (
+        pd.concat(attribute_items)
+        .rename_axis(["attribute", "code"])
+        .rename("label")
+        .to_frame()
+        )
+
+    return attributes
+
+
+def _get_attributes(structure_message):
+    # pandasdmx v1
+    if pandasdmx.__version__.startswith("0.9"):
+        return _get_attributes_0_9(structure_message)
+
+    if pandasdmx.__version__.startswith("1."):
+        return _get_attributes_1(structure_message)
+
+    raise Exception(f"pandasdmx version is {pandasdmx.__version__}")
+
+
+
 _SDMX_FILENAME = 'metadata.sdmx.xml'
 
 
@@ -60,23 +131,33 @@ def _download_sdmx(table: str, dst_dir: PathLike) -> None:
     path = dst_dir / _SDMX_FILENAME
     service = conf['sdmx_service_name']
     name = conf['sdmx_datastructure_template'].format(table=table)
-    r = pandasdmx.Request(service)
-    r.datastructure(name).write_source(str(path))
 
+    r = pandasdmx.Request(service)
+    r.datastructure(name, tofile=path)
+
+
+def _read_structure_message(path):
+    # Need to support pandasdmx==0.9 because 1.0 is not available for Python3.6
+    if pandasdmx.__version__.startswith("0.9"):
+        req = pandasdmx.Request()
+        structure = req.get(
+            fromfile=str(path),
+            writer='pandasdmx.writer.structure2pd'
+            )
+        return structure.write()
+
+    if pandasdmx.__version__.startswith("1."):
+        return pandasdmx.read_sdmx(path)
+
+    raise Exception(f"pandasdmx version is {pandasdmx.__version__}")
 
 def _read(the_dir: PathLike) -> Mapping[str, pd.DataFrame]:
     path = the_dir / _SDMX_FILENAME
-    req = pandasdmx.Request()
-    structure = req.get(
-        fromfile=str(path),
-        writer='pandasdmx.writer.structure2pd'
-        )
-
-    codelist = structure.write()['codelist']
+    structure_message = _read_structure_message(path)
 
     result = {
-        'dimensions': _get_dimensions(codelist),
-        'attributes': _get_attributes(codelist),
+        'dimensions': _get_dimensions(structure_message),
+        'attributes': _get_attributes(structure_message),
     }
 
     return result
