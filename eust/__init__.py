63466cb2e81593ba4d7873a77d3e7f4b1c478069
Rasmus Einarsson <mr@rasmuseinarsson.se>
2022-09-13 15:42:37 +0200

    Bump version: 0.5.2 → 0.5.3

--- eust/__init__.py
+++ eust/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Rasmus Einarsson"""
 __email__ = "mr@rasmuseinarsson.se"
-__version__ = "__version__ = '0.5.2'"
+__version__ = "__version__ = '0.5.3'"
 
 from eust.core import conf, list_config_paths  # noqa: F401
 
